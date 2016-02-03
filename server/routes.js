0db593884035d75cc411e9db89a457b1f6095dee
Musa Musa <musakunte@gmail.com>
2016-02-03 17:48:09 +0100

    added server, stated api module

---
+++ server/routes.js
@@ -0,0 +1,13 @@
+/**
+ * Main application routes
+ */
+'use strict';
+//var userController = require('./api2/users/controller');
+//var errors = require('./components/errors');
+
+module.exports = function(app) {
+
+  // Insert routes below
+  //app.use('/api/v1/user', require('./api/v1/users'));
+
+};
